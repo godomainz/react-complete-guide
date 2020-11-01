576277f8dfca6134cefc828cda84f4916ba45dfc
Akila Mendis <akila@oneit.com.au>
2020-11-01 14:36:13 +0530

    60 Flexible lists

--- src/App.js
+++ src/App.js
@@ -10,8 +10,20 @@ class App extends Component {
   }
 
  
-  nameChangedHandler = (event) =>{
-    this.setState({persons:[{name:"Max", age:28},{name:event.target.value, age:29},{name:"Stephanie", age:26}]});
+  nameChangedHandler = (event,id) =>{
+    const personIndex = this.state.persons.findIndex(p =>
+      {
+        return p.id === id;
+      });
+
+    const person = {...this.state.persons[personIndex]};
+    person.name  = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    
+
+    this.setState({persons: persons});
   }
 
   deletePersonHandler = (personIndex)=>{
@@ -41,7 +53,16 @@ class App extends Component {
         <div>
             {
               this.state.persons.map((person, index)=>{
-                return <Person click={()=>this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id}></Person>
+                return(
+                  <Person 
+                    click={()=>this.deletePersonHandler(index)} 
+                    name={person.name} 
+                    age={person.age} 
+                    key={person.id}
+                    changed={(event)=>this.nameChangedHandler(event, person.id)}
+                  >
+                  </Person>
+                );
               })
             }
         </div> 
