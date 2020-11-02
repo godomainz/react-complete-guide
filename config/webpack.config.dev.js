6792596297d94a0255473e6a1f3aab897e9b0f5e
Akila Mendis <akila@oneit.com.au>
2020-11-02 20:53:17 +0530

    73 Working with css : edit webpack.config.dev.js and webpack.config.prod.js

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -165,7 +165,9 @@ module.exports = {
               {
                 loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1
+                  importLoaders: 1,
+                  modules:true,
+                  localIndentName:'[name]__[local]__[hash:base64:5]'
                 },
               },
               {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -177,7 +177,9 @@ module.exports = {
                       options: {
                         importLoaders: 1,
                         minimize: true,
-                        sourceMap: shouldUseSourceMap
+                        sourceMap: shouldUseSourceMap,
+                        modules:true,
+                        localIndentName:'[name]__[local]__[hash:base64:5]'
                       },
                     },
                     {
