23b660d929085fc09b98ec047e37cebc1f3753c9
Akila Mendis <akila@oneit.com.au>
2020-11-04 19:10:31 +0530

    removed ErrorBoundaries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import classes from  './App.css';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import Person from './Person/Person';
 
 
@@ -51,17 +50,14 @@ class App extends Component {
             {
               this.state.persons.map((person, index)=>{
                 return(
-                  <ErrorBoundary key={person.id}>
                       <Person 
-                      click={()=>this.deletePersonHandler(index)} 
-                      name={person.name} 
-                      age={person.age} 
-                      changed={(event)=>this.nameChangedHandler(event, person.id)}
+                        click={()=>this.deletePersonHandler(index)} 
+                        name={person.name} 
+                        age={person.age} 
+                        changed={(event)=>this.nameChangedHandler(event, person.id)}
+                        key={person.id}
                       >
-                    </Person>
-
-                  </ErrorBoundary>
-                  
+                    </Person>            
                 );
               })
             }
