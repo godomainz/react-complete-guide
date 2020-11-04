045175953a4ba483fbb472bfbdc458192674b6fe
Akila Mendis <akila@oneit.com.au>
2020-11-04 20:50:09 +0530

    88 class based vs functional components

--- src/containers/App.js
+++ src/containers/App.js
@@ -52,7 +52,7 @@ class App extends Component {
     return (
 
         <div className={classes.App}>
-            <Cockpit showPersons={this.state.showPersons} persons={this.state.persons} clicked={this.togglePersonHandler}></Cockpit>
+            <Cockpit title={this.props.appTitle} showPersons={this.state.showPersons} persons={this.state.persons} clicked={this.togglePersonHandler}></Cockpit>
             {persons }
         </div>
 
