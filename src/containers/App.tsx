5361cc21574c592f7a1e47c7bcbb09a7d4f94bf0
Akila Mendis <akila@oneit.com.au>
2020-11-08 17:34:00 +0530

    105 Higher Order components Intro

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import classes from  './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from  '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
+
 interface Props {
   appTitle: string;
 
@@ -81,14 +83,14 @@ class App extends Component<Props> {
     
     
     return (
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
             <button onClick={()=>{this.setState({ showCockpit:false })}}>Remove cockpit</button>
             {this.state.showCockpit ? <Cockpit title={this.props.appTitle} showPersons={this.state.showPersons} personsLength={this.state.persons.length} clicked={this.togglePersonHandler}>
             </Cockpit>:null
             }
             
             {persons }
-        </div>
+        </WithClass>
 
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,"Does this work now ?"));
