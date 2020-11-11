4972c94f6d846af0cfdc215351e6fa2c027b986c
Akila Mendis <akila@oneit.com.au>
2020-11-11 21:15:53 +0530

    125 Starting implementation of the burger builder container

--- burger/src/components/Layout/Layout.tsx
+++ burger/src/components/Layout/Layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Aux from "../../hoc/Auxilary";
-
+import classes from "./Layout.module.css";
 const Layout = (props: any) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
