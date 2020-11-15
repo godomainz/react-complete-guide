467c3fd524ddb79e7eeaa08b41ab393a44f9c7d8
Akila Mendis <akila@oneit.com.au>
2020-11-15 15:24:42 +0530

    146 Creating a Responsive SideDrawer

--- burger/src/components/Layout/Layout.tsx
+++ burger/src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Auxilary";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 const Layout = (props: any) => (
   <Aux>
-    <Toolbar></Toolbar>
+    <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
