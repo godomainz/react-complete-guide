95de3778d077a571696b66d4d531a7f8a1191136
Akila Mendis <akila@oneit.com.au>
2020-11-15 15:57:21 +0530

    148 More about responsive adjustments

--- burger/src/components/Navigation/Toolbar/Toolbar.tsx
+++ burger/src/components/Navigation/Toolbar/Toolbar.tsx
@@ -11,7 +11,7 @@ const  Toolbar = (props:any) => {
           <div className={classes.Logo}>
             <Logo />
           </div>
-          <nav>
+          <nav className={classes.DesktopOnly}>
               <NavigationItems />
           </nav>
       </header>
