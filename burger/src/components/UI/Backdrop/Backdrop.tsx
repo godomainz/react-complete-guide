ccdc1f827b2c0cafee7b6f7c86aff9d5caeb878d
Akila Mendis <akila@oneit.com.au>
2020-11-15 18:11:09 +0530

    Removed console.log from Backdrop

--- burger/src/components/UI/Backdrop/Backdrop.tsx
+++ burger/src/components/UI/Backdrop/Backdrop.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import classes from './Backdrop.module.css';
 
-const  BackDrop = (props:any) => {
-      console.log(props);
+const BackDrop = (props:any) => {
      return(
         props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
      );
-
 }
 
 export default BackDrop;
