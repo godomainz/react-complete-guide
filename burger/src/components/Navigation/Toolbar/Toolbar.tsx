e0aca88744147805cf8eafaa29997706098ad7bc
Akila Mendis <akila@oneit.com.au>
2020-11-15 15:07:56 +0530

    Adding Reusable navigation items

--- burger/src/components/Navigation/Toolbar/Toolbar.tsx
+++ burger/src/components/Navigation/Toolbar/Toolbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.module.css';
 
 const  Toolbar = (props:any) => {
@@ -8,7 +9,9 @@ const  Toolbar = (props:any) => {
       <header className={classes.Toolbar}>
           <div>MENU</div>
           <Logo />
-          <nav>....</nav>
+          <nav>
+              <NavigationItems />
+          </nav>
       </header>
      );
 
