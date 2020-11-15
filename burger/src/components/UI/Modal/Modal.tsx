d5a9638dd69c57f3736367e175a41e3c1f544c60
Akila Mendis <akila@oneit.com.au>
2020-11-15 17:46:36 +0530

    154 Improving performance

--- burger/src/components/UI/Modal/Modal.tsx
+++ burger/src/components/UI/Modal/Modal.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from "react";
 import Aux from '../../../hoc/Auxilary';
 import BackDrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 
+interface Props {
+   show: boolean;
+   modalClosed: any;
+}
 
-const  Modal = (props:any) => (
+class Modal extends Component<Props> {
+
+    shouldComponentUpdate(nextProps:any,nextState:any){
+        return nextProps.show !== this.props.show            
+    }
+
+    componentDidUpdate(){
+        console.log('[Modal.tsx] componentDidUpdate');
+    }
+
+    render(){
+ 
+        return (
+            <Aux>
+                <BackDrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal} style={
+                    {
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' :'0'
+                    }
+                }>
+                {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
     
-    <Aux>
-        <BackDrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal} style={
-            {
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' :'0'
-            }
-        }>
-        {props.children}
-        </div>
-    </Aux>
-    
-)
+}
 
 export default Modal;
