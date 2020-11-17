3f0ab36963f1357ee9e5479b2a2a1ab140c881cc
Akila Mendis <akila@oneit.com.au>
2020-11-17 17:08:08 +0530

    181 Sending a POST Request

--- burger/src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -49,7 +50,7 @@ class BurgerBuilder extends Component {
     const priceAddition = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
-    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    this.setState({totalPrice: Math.round((newPrice + Number.EPSILON) * 100) / 100, ingredients: updatedIngredients});
     this.updatePurchaseState(updatedIngredients);
   }
 
@@ -67,7 +68,7 @@ class BurgerBuilder extends Component {
     const priceAddition = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceAddition;
-    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    this.setState({totalPrice: Math.round((newPrice + Number.EPSILON) * 100) / 100, ingredients: updatedIngredients});
     this.updatePurchaseState(updatedIngredients);
 
   }
@@ -81,7 +82,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseontinueHandler = () => {
-    alert('You Continue');
+    // alert('You Continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Akila Mendis',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '41351',
+          country: 'Sri Lanka'
+        },
+        email: 'test@test.com',
+        deliveryMethod: 'fastest'
+      }
+    };
+
+    axios.post('/orders.json', order)
+          .then(response => console.log(response))
+          .catch(error => console.log(error));
   }
 
   render() {
