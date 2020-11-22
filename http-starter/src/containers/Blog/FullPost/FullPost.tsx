2bdf28512e275c42e63ddebfe6e97549e2d56124
Akila Mendis <akila@oneit.com.au>
2020-11-22 14:08:41 +0530

    203 Extracting Route Parameters

--- http-starter/src/containers/Blog/FullPost/FullPost.tsx
+++ http-starter/src/containers/Blog/FullPost/FullPost.tsx
@@ -4,6 +4,7 @@ import classes from './FullPost.module.css';
 
 interface Props {
     id:number;
+    match?:any;
 }
 interface State {
     loadedPost:any
@@ -15,10 +16,11 @@ class FullPost extends Component<Props> {
         loadedPost:null
     }
 
-    componentDidUpdate(){
-        if(this.props.id){
-            if( !this.state.loadedPost || (this.state.loadedPost && (this.state.loadedPost.id !== this.props.id))){
-                axios.get('/posts/'+this.props.id).then((response)=>{
+    componentDidMount(){
+        console.log(this.props.match.params.id);
+        if(this.props.match.params.id){
+            if( !this.state.loadedPost || (this.state.loadedPost && (this.state.loadedPost.id !== this.props.match.params.id))){
+                axios.get('/posts/'+this.props.match.params.id).then((response)=>{
                     // console.log(response);
                     this.setState({loadedPost: response.data});
                 });
@@ -27,14 +29,14 @@ class FullPost extends Component<Props> {
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/'+this.props.id).then((responce)=>{
+        axios.delete('/posts/'+this.props.match.params.id).then((responce)=>{
             console.log(responce);
         });
     }
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if(this.props.id){
+        if(this.props.match.params.id){
             post = <p style={{textAlign: 'center'}}>Loading....!</p>;
         }
         if(this.state.loadedPost){
