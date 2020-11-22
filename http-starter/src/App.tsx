2a217b41d058265c5921adf78b923e5430043912
Akila Mendis <akila@oneit.com.au>
2020-11-22 18:48:37 +0530

    217 Routing and Server Deployment

--- http-starter/src/App.tsx
+++ http-starter/src/App.tsx
@@ -4,7 +4,8 @@ import Blog from './containers/Blog/Blog';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter>
+    // <BrowserRouter basename="/">
+    <BrowserRouter >
       <div className="App">
         <Blog />
       </div>
