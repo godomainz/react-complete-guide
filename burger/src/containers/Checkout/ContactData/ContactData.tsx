3abf89dfd96409565cda75b913481e74ac9e1b09
Akila Mendis <akila@oneit.com.au>
2020-12-19 18:03:35 +0530

    243 Fixing a Common Validation Gotcha

--- burger/src/containers/Checkout/ContactData/ContactData.tsx
+++ burger/src/containers/Checkout/ContactData/ContactData.tsx
@@ -197,15 +197,15 @@ class ContactData extends Component<Props, State> {
     }
 
     checkValidity(value:string, rules:any){
-        let isValid = false;
+        let isValid = true;
         if (rules.required){
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid;
         }
         if (rules.minLength){
-            isValid = value.trim().length >= rules.minLength;
+            isValid = value.trim().length >= rules.minLength && isValid;
         }
         if (rules.maxLength){
-            isValid = value.trim().length <= rules.maxLength;
+            isValid = value.trim().length <= rules.maxLength && isValid;
         }
         return isValid;
     }
