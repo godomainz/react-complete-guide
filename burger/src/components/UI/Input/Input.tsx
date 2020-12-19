3d7f94a9c2405c7aa8267c2e64912699037089f1
Akila Mendis <akila@oneit.com.au>
2020-12-19 11:31:45 +0530

    240 Handling User Input

--- burger/src/components/UI/Input/Input.tsx
+++ burger/src/components/UI/Input/Input.tsx
@@ -6,14 +6,14 @@ const Input = (props:any) => {
 
     switch (props.elementType){
         case ('input'):
-            inputElement = <input  className={classes.InputElement}  {...props.elementConfig} value={props.value}/>;
+            inputElement = <input  className={classes.InputElement}  {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('textarea'):
-            inputElement = <textarea  className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <textarea  className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('select'):
             inputElement = (
-                            <select  className={classes.InputElement} {...props.elementConfig} value={props.value}>
+                            <select  className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}>
                                 {
                                     props.elementConfig.options.map((option:any) => (
                                         <option key={option.value} value={option.value}>{option.displayValue}</option>
@@ -23,7 +23,7 @@ const Input = (props:any) => {
                             );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} oncChange={props.changed}/>;
             break;
 
     }
--- burger/src/containers/Checkout/ContactData/ContactData.tsx
+++ burger/src/containers/Checkout/ContactData/ContactData.tsx
@@ -144,6 +144,21 @@ class ContactData extends Component<Props, State> {
             });
     }
 
+    inputChangedHandler = (event:any,inputIdentifier:string) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render(){
 
         const formElementsArray = [];
@@ -162,7 +177,8 @@ class ContactData extends Component<Props, State> {
                             key={formElement.id}
                             elementType={formElement.config.elementType} 
                             elementConfig={formElement.config.elementConfig} 
-                            value={formElement.config.value}>
+                            value={formElement.config.value}
+                            changed={(event:any) => this.inputChangedHandler(event,formElement.id)}>
                         </Input>
                     )
                 }
