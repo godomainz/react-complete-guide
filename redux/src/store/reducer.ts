5a4bba260c8eec3525e2e6f2997e37a1c8ade0da
Akila Mendis <akila@oneit.com.au>
2020-12-20 14:48:16 +0530

    262 Switch-Case in the Reducer

--- redux/src/store/reducer.ts
+++ redux/src/store/reducer.ts
@@ -1,29 +1,33 @@
 import CounterState from "./counterState";
 import {ActionTypes,INCREMENT,DECREMENT,ADD,SUBSTRACT} from "./actionTypes"
+
 const initialState: CounterState = {
     counter: 0
 }
+
 const reducer = (state:CounterState=initialState, action:ActionTypes) => {
-    if(action.type ===  INCREMENT){
-        return {
-            counter: state.counter + 1
-        }
+    switch (action.type){
+        case INCREMENT:
+            return {
+                counter: state.counter + 1
+            }
+        case DECREMENT:
+            return {
+                counter: state.counter - 1
+            }
+        case ADD:
+            return {
+                counter: state.counter + action.val
+            }
+        case SUBSTRACT:
+            return {
+                counter: state.counter - action.val
+            }
+        default:
+            return {
+                counter: 0
+            }
     }
-    if(action.type ===  DECREMENT){
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if(action.type ===  ADD){
-        return {
-            counter: state.counter + action.val
-        }
-    }
-    if(action.type ===  SUBSTRACT){
-        return {
-            counter: state.counter - action.val
-        }
-    }
-    return state;
 }
+
 export default reducer;
