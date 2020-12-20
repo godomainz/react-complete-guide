ade4782596dace17b44bda8bc689e4a00e0b777e
Akila Mendis <akila@oneit.com.au>
2020-12-20 10:56:09 +0530

    256 Dispatching Actions

--- redux/redux-basics.ts
+++ redux/redux-basics.ts
@@ -5,6 +5,18 @@ const initialState = { counter:0 }
 
 // Reducer
 const rootReducer = (state=initialState,action)=> {
+    if(action.type === "INC_COUNTER"){
+        return {
+            ...state, 
+            counter: state.counter + 1
+        }
+    }
+    if(action.type === "ADD_COUNTER"){
+        return {
+            ...state, 
+            counter: state.counter + action.value
+        }
+    }
     return state;
 };
 
@@ -13,5 +25,9 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({type: "INC_COUNTER"});
+console.log(store.getState());
+store.dispatch({type: "ADD_COUNTER", value: 10});
+console.log(store.getState());
 
 // Subscription
