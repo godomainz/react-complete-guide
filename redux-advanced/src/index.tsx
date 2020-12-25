52cbcada041bf3acce4970b0569a56fe35c06ec8
Akila Mendis <akila@oneit.com.au>
2020-12-25 11:57:21 +0530

    285 Using the Redux Devtools

--- redux-advanced/src/index.tsx
+++ redux-advanced/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {createStore, combineReducers, applyMiddleware } from "redux";
+import {createStore, combineReducers, applyMiddleware, compose } from "redux";
 import {Provider} from "react-redux";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -24,7 +24,14 @@ const logger = (store:any) => {
   }
 }
 
-const store = createStore(rootReducer,applyMiddleware(logger));
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <React.StrictMode>
