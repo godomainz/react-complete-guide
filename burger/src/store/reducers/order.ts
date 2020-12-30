d31e8e78c2fee940297d3e95af37f96b636b9d69
Akila Mendis <akila@oneit.com.au>
2020-12-30 10:43:14 +0530

    309 The Order Reducer

--- burger/src/store/reducers/order.ts
+++ burger/src/store/reducers/order.ts
@@ -1,1 +1,31 @@
-import * as actionTypes from "./burgerBuilder";
+import * as actionTypes from "../actions/actionTypes";
+import OrderState from "../orderState";
+
+const initialState:OrderState = {
+    orders:[],
+    loading: false
+}
+
+const reducer = (state:OrderState=initialState, action:actionTypes.ActionTypes) => {
+    switch(action.type){
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderID
+            }
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder)
+            }
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
