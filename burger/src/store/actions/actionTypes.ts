8bf246f493c242452f5cce158339c3aff739e90c
Akila Mendis <akila@oneit.com.au>
2020-12-30 09:53:24 +0530

    307 Adding Order Actions

--- burger/src/store/actions/actionTypes.ts
+++ burger/src/store/actions/actionTypes.ts
@@ -4,9 +4,14 @@ export const REMOVE_INGREDIENT = "REMOVE_INGREDIENT";
 export const SET_INGREDIENTS = "SET_INGREDIENTS";
 export const FETCH_INGREDIENTS_FAILED = "FETCH_INGREDIENT_FAILED";
 
-export type AddIngredientAction = {type: typeof ADD_INGREDIENT, ingredientName:string}
-export type RemoveIngredientAction = {type: typeof REMOVE_INGREDIENT, ingredientName:string}
-export type SetIngredientsAction = {type: typeof SET_INGREDIENTS, ingredients:Ingredients}
-export type FetchIngredientsFailedAction = {type: typeof FETCH_INGREDIENTS_FAILED}
+export const PURCHASE_BURGER_SUCCESS = "PURCHASE_BURGER_SUCCESS";
+export const PURCHASE_BURGER_FAIL = "PURCHASE_BURGER_FAIL";
 
-export type ActionTypes = AddIngredientAction | RemoveIngredientAction | SetIngredientsAction | FetchIngredientsFailedAction;
+export type AddIngredientAction = {type: typeof ADD_INGREDIENT, ingredientName:string};
+export type RemoveIngredientAction = {type: typeof REMOVE_INGREDIENT, ingredientName:string};
+export type SetIngredientsAction = {type: typeof SET_INGREDIENTS, ingredients:Ingredients};
+export type FetchIngredientsFailedAction = {type: typeof FETCH_INGREDIENTS_FAILED};
+export type PurchaseBurgerSuccessAction = {type: typeof PURCHASE_BURGER_SUCCESS, orderID:number, orderData:any}
+export type PurchaseBurgerFailAction = {type: typeof PURCHASE_BURGER_FAIL,error:string}
+
+export type ActionTypes = AddIngredientAction | RemoveIngredientAction | SetIngredientsAction | FetchIngredientsFailedAction | PurchaseBurgerSuccessAction | PurchaseBurgerFailAction;
--- burger/src/store/actions/order.ts
+++ burger/src/store/actions/order.ts
@@ -1,1 +1,27 @@
 import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
+
+export const purchaseBurgerSuccess = (id:number, orderData:any):actionTypes.PurchaseBurgerSuccessAction => ({
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderID: id,
+    orderData: orderData
+});
+
+export const purchaseBurgerFail = (error:string):actionTypes.PurchaseBurgerFailAction => ({
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    error: error
+});
+
+export const purchaseBurgerStart = (orderData:any) => {
+    return (dispatch:(func:any)=>void) => {
+        axios.post('/orders.json', orderData)
+            .then(response => {
+                console.log(response.data);
+                dispatch(purchaseBurgerSuccess(response.data,orderData));
+                
+            })
+            .catch(error => {
+                dispatch(purchaseBurgerFail(error.toString()));
+            });
+    }
+};
