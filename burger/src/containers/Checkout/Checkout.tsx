b50d65d209a8e61441e48fc6f82cb15b8ee0a9ea
Akila Mendis <akila@oneit.com.au>
2020-12-31 11:38:54 +0530

    removed unwanted line

--- burger/src/containers/Checkout/Checkout.tsx
+++ burger/src/containers/Checkout/Checkout.tsx
@@ -3,7 +3,6 @@ import {connect} from 'react-redux';
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import { Route, Redirect } from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
-import * as actions from "../../store/actions";
 
 interface Props {
     history: any;
