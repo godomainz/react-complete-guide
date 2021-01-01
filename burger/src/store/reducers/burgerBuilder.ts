695bbed4fae1f33aebbbf7c0b22c6e774cc8f5a8
Akila Mendis <akila@oneit.com.au>
2021-01-01 17:48:26 +0530

    318 Refactoring Reducers Continued

--- burger/src/store/reducers/burgerBuilder.ts
+++ burger/src/store/reducers/burgerBuilder.ts
@@ -15,45 +15,55 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
   }
   
+const addIngredient = (state:IngredientState, action:actionTypes.AddIngredientAction) => {
+    const upgradedIngredient = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+    } ;
+    const updatedIngredients = updateObject(state.ingredients,upgradedIngredient);
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedState);
+}
+
+const removeIngredient = (state:IngredientState, action:actionTypes.RemoveIngredientAction) => {
+    const upgradedIng = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+    } ;
+    const updatedIngs = updateObject(state.ingredients,upgradedIng);
+    const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+   return updateObject(state, updatedSt);
+}
+
+const setIngredients = (state:IngredientState, action:actionTypes.SetIngredientsAction) => {
+    return updateObject(state, {
+        // ingredients: {
+            //     salad: action.ingredients.salad,
+            //     bacon: action.ingredients.bacon,
+            //     cheese: action.ingredients.bacon,
+            //     meat: action.ingredients.meat,
+            // },
+            ingredients: action.ingredients,
+            totalPrice: initialState.totalPrice,
+            error: false
+        });
+}
+
+const fetchIngredientsFailed = (state:IngredientState, action:actionTypes.FetchIngredientsFailedAction) => {
+    return updateObject(state, { error: true });
+}
 
 const reducer = (state:IngredientState=initialState, action:actionTypes.ActionTypes) => {
     switch (action.type){
-        case actionTypes.ADD_INGREDIENT:
-            const upgradedIngredient = {
-                [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-            } ;
-            const updatedIngredients = updateObject(state.ingredients,upgradedIngredient);
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-           return updateObject(state, updatedState);
-        case actionTypes.REMOVE_INGREDIENT:
-            const upgradedIng = {
-                [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-            } ;
-            const updatedIngs = updateObject(state.ingredients,upgradedIng);
-            const updatedSt = {
-                ingredients: updatedIngs,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-           return updateObject(state, updatedSt);
-        case actionTypes.SET_INGREDIENTS:
-            return updateObject(state, {
-                // ingredients: {
-                    //     salad: action.ingredients.salad,
-                    //     bacon: action.ingredients.bacon,
-                    //     cheese: action.ingredients.bacon,
-                    //     meat: action.ingredients.meat,
-                    // },
-                    ingredients: action.ingredients,
-                    totalPrice: initialState.totalPrice,
-                    error: false
-                });
-        case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return updateObject(state, { error: true });
-        default:
-            return state
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state,action);
+        case actionTypes.SET_INGREDIENTS: return setIngredients(state,action);
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action);
+        default: return state
     }
 }
 
--- burger/src/store/reducers/order.ts
+++ burger/src/store/reducers/order.ts
@@ -8,14 +8,16 @@ const initialState:OrderState = {
     purchased: false
 }
 
-const reducer = (state:OrderState=initialState, action:actionTypes.ActionTypes) => {
-    switch(action.type){
-        case actionTypes.PURCHASE_INIT:
-            return updateObject(state, { purchased: false });
-        case actionTypes.PURCHASE_BURGER_START:
-            return updateObject(state, { loading: true });
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = updateObject(action.orderData, { id: action.orderID });
+const purchaseInit = (state:OrderState, action:actionTypes.PurchaseInitAction) => {
+    return updateObject(state, { purchased: false });
+}
+
+const purchaseBurgerStart = (state:OrderState, action:actionTypes.PurchaseBurgerStartAction) => {
+    return updateObject(state, { loading: true });
+}
+
+const purchaseBurgerSuccess = (state:OrderState, action:actionTypes.PurchaseBurgerSuccessAction) => {
+    const newOrder = updateObject(action.orderData, { id: action.orderID });
             return updateObject(state, 
                     { 
                         loading: false,
@@ -23,20 +25,38 @@ const reducer = (state:OrderState=initialState, action:actionTypes.ActionTypes) 
                         orders: state.orders.concat(newOrder) 
                     }
                 );
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return updateObject(state,{ loading: false });
-        case actionTypes.FETCH_ORDERS_START:
-            return updateObject(state,{ loading: true });
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return updateObject(state, {
-                    orders: action.orders,
-                    loading: false
-                }
-            )
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return updateObject(state,{ loading: false });
-        default:
-            return state;
+}
+
+const purchaseBurgerFail = (state:OrderState, action:actionTypes.PurchaseBurgerFailAction) => {
+    return updateObject(state,{ loading: false });
+}
+
+const fetchorderStart = (state:OrderState, action:actionTypes.FetchOrdersStartAction) => {
+    return updateObject(state,{ loading: true });
+}
+
+const fetchordersSuccess = (state:OrderState, action:actionTypes.FetchOrdersSuccessAction) => {
+    return updateObject(state, {
+            orders: action.orders,
+            loading: false
+        }
+    )
+}
+
+const fetchordersFail = (state:OrderState, action:actionTypes.FetchOrdersFailAction) => {
+    return updateObject(state,{ loading: false });
+}
+
+const reducer = (state:OrderState=initialState, action:actionTypes.ActionTypes) => {
+    switch(action.type){
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+        case actionTypes.FETCH_ORDERS_START: return fetchorderStart(state, action);
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchordersSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchordersFail(state, action);
+        default: return state;
     }
 }
 
