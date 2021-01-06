48c79065f3af2c07126a15259e0f1bcf18f9ed24
Akila Mendis <akila@oneit.com.au>
2021-01-06 14:22:06 +0530

    346 Sharing the Validation Method

--- burger/src/containers/Checkout/ContactData/ContactData.tsx
+++ burger/src/containers/Checkout/ContactData/ContactData.tsx
@@ -8,7 +8,7 @@ import Input from "../../../components/UI/Input/Input";
 import { OrderForm, OrderFormModel } from "./OrderForm";
 import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from "../../../store/actions/index";
-import { updateObject } from "../../../shared/utility";
+import { updateObject, checkValidity } from "../../../shared/utility";
 
 interface Props {
     ings:any;
@@ -49,44 +49,12 @@ class ContactData extends Component<Props, State> {
         this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidity(value:string, rules:any){
-        let isValid = true;
-        console.log(value);
-        console.log(rules);
-        if(!rules){
-            return true;
-        }
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-        return isValid;
-    }
-
     inputChangedHandler = (event:any,inputIdentifier:string) => {
-        
+
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
-            value:event.target.value,
-            valid:this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
-            touched:true
+            value: event.target.value,
+            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            touched: true
         });
 
         const updatedOrderForm = updateObject(this.state.orderForm, {
