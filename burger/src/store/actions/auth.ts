e78023c0b42c10a443d2fbf23eda05099c60b436
Akila Mendis <akila@oneit.com.au>
2021-01-06 10:20:42 +0530

    removed unnecessary times

--- burger/src/store/actions/auth.ts
+++ burger/src/store/actions/auth.ts
@@ -92,9 +92,8 @@ export const authCheckState = () => {
             }else{
                 const userId = localStorage.getItem('userId');
                 dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout(expirationDate.getSeconds() -  new Date().getSeconds()))
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
             }
-            
         }
     }
 }
