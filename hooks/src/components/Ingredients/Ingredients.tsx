c4d09ff8df21ea02650533d37cf2e6979a782f2d
Akila Mendis <akila@oneit.com.au>
2021-01-07 10:36:41 +0530

    434 useEffect()  Loading Data

--- hooks/src/components/Ingredients/Ingredients.tsx
+++ hooks/src/components/Ingredients/Ingredients.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
 import Search from './Search';
@@ -6,6 +6,22 @@ import { Ingredient } from "./Ingredient"
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState<Ingredient[]>([])
+
+  useEffect(() =>{
+    fetch("https://react-hooks-update-6f530-default-rtdb.firebaseio.com/ingredients.json").then(response => {
+      return response.json();
+    }).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData){
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    });
+  },[]);
 
   const addIngredientHandler = (ingredient:Ingredient) => {
     fetch("https://react-hooks-update-6f530-default-rtdb.firebaseio.com/ingredients.json",{
