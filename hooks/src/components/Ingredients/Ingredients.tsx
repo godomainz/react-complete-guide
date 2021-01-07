abec4c52e2bb79f36cb1f4ae53248a8ff65f01fe
Akila Mendis <akila@oneit.com.au>
2021-01-07 10:19:21 +0530

    433 Sending Http Requests

--- hooks/src/components/Ingredients/Ingredients.tsx
+++ hooks/src/components/Ingredients/Ingredients.tsx
@@ -8,13 +8,20 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState<Ingredient[]>([])
 
   const addIngredientHandler = (ingredient:Ingredient) => {
-    setUserIngredients((prevIngredients:Ingredient[]) => [...prevIngredients,{
-      id: Math.random().toString(),
-      ...ingredient
-    }]);
-    console.log(ingredient);
-    console.log(userIngredients);
+    fetch("https://react-hooks-update-6f530-default-rtdb.firebaseio.com/ingredients.json",{
+      method:"POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json;" }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients((prevIngredients:Ingredient[]) => [...prevIngredients,{
+        id: responseData.name,
+        ...ingredient
+      }]);
+    });
   }
+
   const removeIngredientHandler = (id:string) => {
     setUserIngredients((prevIngredients:Ingredient[])=>{
       return prevIngredients.filter((ingredient:Ingredient)=> ingredient.id !== id )
