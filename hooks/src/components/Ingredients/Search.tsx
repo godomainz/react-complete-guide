ab686e43abb9097e0490f7d7538c3fed874d8759
Akila Mendis <akila@oneit.com.au>
2021-01-08 13:20:40 +0530

    438 Working with Refs  useRef()

--- hooks/src/components/Ingredients/Search.tsx
+++ hooks/src/components/Ingredients/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import classes from './Search.module.css';
@@ -11,30 +11,38 @@ interface Iprops {
 const Search = React.memo((props:Iprops) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 ? "" : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch("https://react-hooks-update-6f530-default-rtdb.firebaseio.com/ingredients.json"+query).then(response => {
-      return response.json();
-    }).then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData){
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
-      }
-      onLoadIngredients(loadedIngredients);
-    });
-  },[enteredFilter, onLoadIngredients]);
+    setTimeout(()=>{
+      if(inputRef.current){
+        if(enteredFilter === inputRef.current.value){
+          const query = enteredFilter.length === 0 ? "" : `?orderBy="title"&equalTo="${enteredFilter}"`;
+          fetch("https://react-hooks-update-6f530-default-rtdb.firebaseio.com/ingredients.json"+query).then(response => {
+            return response.json();
+          }).then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData){
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
+        }
+      }   
+    } ,500);
+    
+  },[enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className={classes.Search}>
       <Card>
         <div className={classes.SearchInput}>
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={event=>setEnteredFilter(event.target.value)} />
+          <input ref={inputRef} type="text" value={enteredFilter} onChange={event=>setEnteredFilter(event.target.value)} />
         </div>
       </Card>
     </section>
